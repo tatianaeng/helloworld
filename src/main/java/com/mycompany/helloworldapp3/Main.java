61968e39ff2164b7a108d1662a7e6966e9a88bab
tatianaeng <tatianaeng@tatianas-air.lan>
2023-10-17 21:21:04 -0400

    Hello World app

---
+++ src/main/java/com/mycompany/helloworldapp3/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.helloworldapp3;
+
+/**
+ *
+ * @author tatianaeng
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world from Tatiana Eng");
+    }
+}
